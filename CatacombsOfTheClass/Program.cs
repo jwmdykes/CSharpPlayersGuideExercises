9ae966ae153f0170a8e0cdf69492bae29a2a00fd
John Dykes <98johndykes@gmail.com>
2023-11-17 08:03:07 -0500

    do The Locked Door

--- CatacombsOfTheClass/Program.cs
+++ CatacombsOfTheClass/Program.cs
@@ -1,40 +1,103 @@
-﻿
-Point p1 = new(5f, 3f);
-p1.X = 15;
-Point p2 = new();
+﻿// Point p1 = new(5f, 3f);
+// p1.X = 15;
+// Point p2 = new();
+//
+// Console.ForegroundColor = ConsoleColor.Red;
+// Console.WriteLine(p1);
+// Console.WriteLine(p2);
+//
+// Color c1 = new() { R = 255, G = 0, B = 128 };
+// Color c2 = new(1, 2, 3);
+// Color c3 = Color.Orange;
+//
+// Console.ForegroundColor = ConsoleColor.Yellow;
+// Console.WriteLine(c1);
+// Console.WriteLine(c2);
+// Console.WriteLine(c3);
 
-Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine(p1);
-Console.WriteLine(p2);
+int deckSize = Enum.GetValues(typeof(CardColor)).Length * Enum.GetValues(typeof(CardRank)).Length;
+var deck = new Card[deckSize];
 
-Color c1 = new() { R = 255, G = 0, B = 128 };
-Color c2 = new(1, 2, 3);
-Color c3 = Color.Orange;
+var index = 0;
+foreach (CardColor color in Enum.GetValues(typeof(CardColor)))
+{
+    foreach (CardRank rank in Enum.GetValues(typeof(CardRank)))
+    {
+        deck[index] = new Card() { Color = color, Rank = rank };
+        index++;
+    }
+}
 
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine(c1);
-Console.WriteLine(c2);
-Console.WriteLine(c3);
+foreach (var card in deck)
+{
+    Console.WriteLine(card);
+}
 
+
+internal enum CardColor
+{
+    Red,
+    Green,
+    Blue,
+    Yellow
+}
+
+internal enum CardRank
+{
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Dollar,
+    Percent,
+    Carrot,
+    Ampersand
+}
+
+internal class Card
+{
+    public override string ToString() => $"{Color} {Rank} card.";
+    
+    public CardColor Color { set; get; }
+    public CardRank Rank { set; get; }
+
+    public bool IsSymbolCard => Rank switch
+    {
+        CardRank.Dollar => true,
+        CardRank.Percent => true,
+        CardRank.Carrot => true,
+        CardRank.Ampersand => true,
+        _ => false
+    };
+}
 
 internal class Color
 {
-    public static readonly Color White = new (0, 0, 0);
-    public static readonly Color Black = new (0xff, 0xff, 0xff);
-    public static readonly Color Red = new (0xff, 0, 0);
-    public static readonly Color Blue = new (0, 0, 0xff);
-    public static readonly Color Green = new (0, 0xff, 0);
-    public static readonly Color Orange = new (255, 165, 0);
-    public static readonly Color Yellow = new (255, 255, 0);
-    public static readonly Color Purple = new (128, 0, 128);
-        
+    public static readonly Color White = new(0, 0, 0);
+    public static readonly Color Black = new(0xff, 0xff, 0xff);
+    public static readonly Color Red = new(0xff, 0, 0);
+    public static readonly Color Blue = new(0, 0, 0xff);
+    public static readonly Color Green = new(0, 0xff, 0);
+    public static readonly Color Orange = new(255, 165, 0);
+    public static readonly Color Yellow = new(255, 255, 0);
+    public static readonly Color Purple = new(128, 0, 128);
+
     public override string ToString() => $"#{R:x2}{G:x2}{B:x2}";
-    
+
     public byte R { get; set; }
     public byte G { get; set; }
     public byte B { get; set; }
 
-    public Color() : this(0, 0, 0) {}
+    public Color() : this(0, 0, 0)
+    {
+    }
+
     public Color(byte r, byte g, byte b)
     {
         (R, G, B) = (r, g, b);
---
+++ TheLockedDoor/Program.cs
@@ -0,0 +1,166 @@
+﻿Console.WriteLine("What would you like the password of the door to start as?");
+string? password = null;
+while (password == null)
+{
+    password = Console.ReadLine();
+}
+
+Door door = new(password);
+int command;
+string? message = null;
+
+do
+{
+    Console.Clear();
+    
+    if (message != null)
+    {
+        Console.WriteLine(message);
+    }
+    
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("----------------------------------------------------");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($"The door is currently {door.State.ToString().ToLower()}.");
+    Console.WriteLine("What would you like to do?");
+
+    Console.WriteLine("1. Open the door");
+    Console.WriteLine("2. Close the door");
+    Console.WriteLine("3. Lock the door");
+    Console.WriteLine("4. Unlock the door");
+    Console.WriteLine("5. Change the password");
+    Console.WriteLine("6. Quit");
+
+    command = Convert.ToInt32(Console.ReadLine());
+
+    switch (command)
+    {
+        case 1:
+            message = door.Open();
+            break;
+        case 2:
+            message = door.Close();
+            break;
+        case 3:
+            message = door.Lock();
+            break;
+        case 4:
+            Console.WriteLine("Enter the password to unlock the door.");
+            password = Console.ReadLine()!;
+            message = door.Unlock(password);
+            break;
+        case 5:
+            Console.WriteLine("Enter the current password to change the password.");
+            string currentPassword = Console.ReadLine()!;
+            Console.WriteLine("Enter the new password");
+            string newPassword = Console.ReadLine()!;
+            message = door.ChangePassword(currentPassword, newPassword);
+            break;
+        case 6:
+            break;
+    }
+} while (command != 6);
+
+internal class Door
+{
+    public enum StateEnum
+    {
+        Open = 0,
+        Closed = 1,
+        Locked = 2
+    }
+
+    private string? _password;
+
+    public Door(string? password)
+    {
+        _password = password;
+        State = StateEnum.Open;
+    }
+
+    public StateEnum State { get; private set; }
+
+    public string ChangePassword(string oldPassword, string? newPassword)
+    {
+        if (oldPassword != _password)
+        {
+            return "The provided password does not match the current password.";
+        }
+
+        _password = newPassword;
+        return "Password changed successfully.";
+    }
+
+    public string Unlock(string password)
+    {
+        switch (State)
+        {
+            case StateEnum.Closed:
+            case StateEnum.Open:
+                return "Door is already unlocked.";
+            case StateEnum.Locked:
+                if (password == _password)
+                {
+                    State = StateEnum.Closed;
+                    return "Unlocking the door.";
+                }
+                else
+                {
+                    return "Incorrect password.";
+                }
+        }
+
+        // This line should not be reachable, but it's here to satisfy the compiler.
+        return "Unknown error.";
+    }
+
+    public string Close()
+    {
+        switch (State)
+        {
+            case StateEnum.Locked:
+            case StateEnum.Closed:
+                return "Door is already closed.";
+            case StateEnum.Open:
+                State = StateEnum.Closed;
+                return "Closing the door.";
+        }
+        
+        // This line should not be reachable, but it's here to satisfy the compiler.
+        return "Unknown error.";
+    }
+
+    public string Open()
+    {
+        switch (State)
+        {
+            case StateEnum.Open:
+                return "Door is already open.";
+            case StateEnum.Closed:
+                State = StateEnum.Open;
+                return "Opening the door.";
+            case StateEnum.Locked:
+                return "The door is locked.";
+        }
+
+        // This line should not be reachable, but it's here to satisfy the compiler.
+        return "Unknown error.";
+    }
+
+    public string Lock()
+    {
+        switch (State)
+        {
+            case StateEnum.Open:
+                return "Need to close the door before you lock it.";
+            case StateEnum.Locked:
+                return "The door is already locked.";
+            case StateEnum.Closed:
+                State = StateEnum.Locked;
+                return "Locking the door.";
+        }
+
+        // This line should not be reachable, but it's here to satisfy the compiler.
+        return "Unknown error.";
+    }
+}
